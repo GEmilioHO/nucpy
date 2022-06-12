7a1860b3f66b47678a5a9c1dd420011cea4b2d81
GEmilioHO <gemilio.herrerao@gmail.com>
2022-06-12 18:47:41 +0100

    Logg info update

--- nucpy/processing/_read_image.py
+++ nucpy/processing/_read_image.py
@@ -10,6 +10,9 @@ def get_array_czi(filename):
     Read image in CZI format as array
     """
 
+    fn = filename.replace(".czi", "").split("/")[-1]
+    logg.info(f"Reading CZI file '{fn}'...")
+
     czi = zis.CziFile(filename)
     cziarray = czi.asarray()
     czi.close()
@@ -22,6 +25,9 @@ def get_array_common(filename):
     Read image in common image format (png, jpeg, jpg)
     """
 
+    fn = filename.replace(".png", "").replace(".jpeg", "").replace(".jpg", "").split("/")[-1]
+    logg.info(f"Reading image file '{fn}'...")
+
     imgarray = image.imread(filename)
 
     return imgarray
@@ -31,6 +37,9 @@ def get_array_tiff(filename):
     """
     Read image in TIFF/TIF format as array
     """
+
+    fn = filename.replace(".tiff", "").replace(".tif", "").split("/")[-1]
+    logg.info(f"Reading image file '{fn}'...")
 
     tiffarray = tifffile.imread(filename)
 
